d03e0e8535832451ad250ae693949f505d194deb
Daniel Inama <daniel.inama02@gmail.com>
2021-03-30 16:45:49 +0200

    now says the nick of who requested the sticker

--- Cogs/sticker/sticker.py
+++ Cogs/sticker/sticker.py
@@ -2,7 +2,11 @@ import discord
 from discord.ext import commands
 import json 
 from random import randint
+from discord.guild import Guild
 
+
+client = discord.Client()
+user = discord.User
 
 
 class StickerCog(commands.Cog, name="sticker command"): 
@@ -14,18 +18,25 @@ class StickerCog(commands.Cog, name="sticker command"):
                     description = "Send a sticker")
     @commands.cooldown(1, 2, commands.BucketType.member)
     async def sticker(self, ctx,sticker:str=None):
+        #searches last 200 messages
+        channel =  ctx.channel
+        guild = ctx.guild
+        async for message in channel.history(limit=200):
+            if ("!sticker " + sticker) in message.content:
+                await message.delete()
+                member = await guild.fetch_member(message.author.id)
+                nick = member.display_name
+                break
+
         if sticker is not None:
             with open("Cogs/sticker/stickers.json", "r") as config: 
                 data = json.load(config)
                 for item in data:
                     if sticker in item: 
+                        await ctx.send(nick + ":")
                         await ctx.send(data[item])
-                channel =  ctx.channel
-            #searches last 200 messages
-            async for message in channel.history(limit=200):
-                if ("!sticker " + sticker) in message.content: 
-                    await message.delete()
-                    break
+
+
         else:
             stickerlist = ""
             with open("Cogs/sticker/stickers.json", "r") as file: 
@@ -33,6 +44,7 @@ class StickerCog(commands.Cog, name="sticker command"):
                 for item in data: 
                     stickerlist += item + ", "
             embed = discord.Embed(title=f"__**List of Stickers**__", description=f"{(stickerlist)}", color=randint(0, 0xffffff))
+            embed.set_thumbnail(url=f"https://cdn.discordapp.com/attachments/822577638098993192/826453061932810312/Instagram-UI_sticker-512.png")
             await ctx.channel.send(embed=embed)
 
 def setup(bot:commands.Bot):
